2c4eab3b859ac5b9a3fab38c5a7f92685461292f
vamphonephong <vamphonephong@gmail.com>
2016-09-25 03:19:37 -0700

    changes to cpu_scheduler.cpp

--- Project1/Part1/cpu_scheduler.cpp
+++ Project1/Part1/cpu_scheduler.cpp
@@ -2,162 +2,297 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define HYPERV_URI 	"qemu:///system"
 #define MAX_CPUS	128
+#define MAX_MAPPING_BYTES (MAX_CPUS >> 3)
 
-struct CPU_STATS
+static virDomainPtr *domains = NULL;
+
+//tracks the load for each PCPU
+static unsigned long long cpuTimes[MAX_CPUS] = {0};
+
+struct virDomainWindow
 {
-	unsigned long long utilizationStats;
-	unsigned long long idleStats;
+	virDomainPtr domain;
+	unsigned short nVcpus;
+	//nPcpus and nParams used by virDomainGetCPUStats only.
+	//not needed for my implementation but will keep it there just in case.
+	int nPcpus;
+	int nParams;
+	virVcpuInfoPtr prevStats;
+	virVcpuInfoPtr currStats;
+	//cpumaps isn't really used, but I'll just keep it here
+	unsigned char *cpumaps;
+	virDomainWindow() :
+		domain(NULL),
+		nVcpus(0),
+		nPcpus(0),
+		nParams(0),
+		prevStats(NULL),
+		currStats(NULL),
+		cpumaps(NULL)
+	{}
 };
 
-int main()
+void destroyDomainWindows(int nDomains, virDomainWindow *domainWindows)
 {
-	int nDomains= 0;
+	printf("Destroying domain windows\n");
+	for (int i = 0; i < nDomains; i++)
+	{
+		virDomainFree(domainWindows[i].domain);
+		if (domainWindows[i].prevStats)
+		{
+			free(domainWindows[i].prevStats);
+		}
+		if (domainWindows[i].currStats)
+		{
+			free(domainWindows[i].currStats);
+		}
+		if (domainWindows[i].cpumaps)
+		{
+			free(domainWindows[i].cpumaps);
+		}
+	}
+}
+//return number of domains and fill output parameter with all domain windows
+int initDomainWindows(virConnectPtr connection, virDomainWindow *&domainWindows)
+{
+	unsigned int flags = VIR_CONNECT_LIST_DOMAINS_RUNNING | VIR_CONNECT_LIST_DOMAINS_PERSISTENT;
+	int nDomains = 0;
+	virDomainInfo domainInfo;
+	
+	if((nDomains = virConnectListAllDomains(connection, &domains, flags)) < 0)
+	{
+		printf("Error getting domains list.\n");
+	}
+
+	domainWindows = (virDomainWindow*)calloc(nDomains, sizeof(virDomainWindow));
+	printf("Number of domains: %d\n", nDomains);
+
+	for (int i = 0; i < nDomains; i++)
+	{
+		domainWindows[i].domain = domains[i];
+		//get number of vcpus in the domain
+		if ((domainWindows[i].nVcpus = virDomainGetVcpusFlags(domainWindows[i].domain, VIR_DOMAIN_VCPU_CURRENT)) < 0)
+		{
+			printf("Error getting number of vCpus for domain %d\n", i);
+			return -1;
+		}	
+		//nParams and nPcpus is not used, but will keep it here for now.
+		//get the number of params available for each pcpu
+		if ((domainWindows[i].nParams = virDomainGetCPUStats(domainWindows[i].domain, NULL, 0, 0, 1, 0)) < 0)
+		{
+			printf("Error getting number of params for each vCpu.\n");
+			return -1;
+		}
+		//get number of pcpus this domain can map to
+		if ((domainWindows[i].nPcpus = virDomainGetCPUStats(domainWindows[i].domain, NULL, 0, 0, 0, 0)) < 0)
+		{
+			printf("Error getting number of pCpus for domain %d\n", i);
+			return -1;
+		}
+		
+		//allocate both previous and current stats arrays
+		domainWindows[i].prevStats = (virVcpuInfoPtr)calloc(domainWindows[i].nVcpus, sizeof(virVcpuInfo));
+		domainWindows[i].currStats = (virVcpuInfoPtr)calloc(domainWindows[i].nVcpus, sizeof(virVcpuInfo));
+
+		domainWindows[i].cpumaps = (unsigned char*)calloc(domainWindows[i].nVcpus, VIR_CPU_MAPLEN(domainWindows[i].nPcpus));
+		printf("domain %s info:\n", virDomainGetName(domainWindows[i].domain));
+		printf("\tnVcpu:%d\n....................\n", domainWindows[i].nVcpus);
+	}
+
+	return nDomains;
+}
+
+int fetchStats(int nDomains, virDomainWindow *domainWindows)
+{
+	printf("Fetching Vcpu stats...");
+	for (int i = 0; i < nDomains; i++)
+	{
+		//copy last stats into previous to so that we can get time interval
+		memcpy(domainWindows[i].prevStats, domainWindows[i].currStats, domainWindows[i].nVcpus*sizeof(virVcpuInfo));
+		if ((virDomainGetVcpus(domainWindows[i].domain, domainWindows[i].currStats, domainWindows[i].nVcpus, domainWindows[i].cpumaps, VIR_CPU_MAPLEN(domainWindows[i].nPcpus))) < 0)
+		{
+			printf("\nError getting VCPU stats. Aborting.\n");
+			return -1;
+		}
+	}
+	printf("done.\n");
+
+	return 0;
+}
+
+int getNextPcpuIndex(unsigned long long *cpuTimes, int max, int startIndex=0)
+{
+	printf("max: %d\n", max);
+	int pcpuPin = startIndex%max;
+	printf("startIndex: %d\n", pcpuPin);
+	
+	for (int i = 0; i < max; i++)
+	{
+		//just return the index i if value here is 0
+		if (cpuTimes[(i+startIndex)%max] == 0)
+		{
+			printf("new cpu\n");
+			return (i+startIndex)%max;
+		}
+		
+		pcpuPin = cpuTimes[(i+startIndex)%max] < cpuTimes[pcpuPin] ? (i+startIndex)%max : pcpuPin;
+	}
+	return pcpuPin;
+}
+
+void printCpuMapping(int nDomains, virDomainWindow *domainWindows)
+{
+	virVcpuInfoPtr infoPtr = NULL; 
+
+	for (int i = 0; i < nDomains; i++)
+	{
+		infoPtr = (virVcpuInfoPtr)calloc(domainWindows[i].nVcpus, sizeof(virVcpuInfo));
+		virDomainGetVcpus(domainWindows[i].domain, infoPtr, domainWindows[i].nVcpus, NULL, 0);
+		printf("\n\nDomain %s mapping:\n", virDomainGetName(domainWindows[i].domain));
+		for (int j = 0; j < domainWindows[i].nVcpus; j++)
+		{
+			printf("vcpu%d\t", infoPtr[j].number);
+		}
+		printf("\n---------------------------\n");
+		for (int j = 0; j < domainWindows[i].nVcpus; j++)
+		{
+			printf("%d\t", infoPtr[j].cpu);
+		}
+		printf("\n\n");
+		free(infoPtr);
+	}
+}
+
+void setNewPinMappings(int nDomains, virDomainWindow *domainWindows)
+{
+	printf("=============================================\n");
+	printf("\n\nSetting new pin mappings\n");
+	printf("\n-------------------------------------------\n");
+	
+	//copy cpu usage table to represent expected workload
+	unsigned long long expectedWorkload[MAX_CPUS];
+	memcpy(expectedWorkload, cpuTimes, MAX_CPUS);
+	//pin mapping bytes
+	unsigned char mappings[MAX_MAPPING_BYTES];
+	int pin = 0;
+	int maxNPcpus = 0;
+
+	//go through each vcpu and map the pins
+	for (int i = 0; i < nDomains; i++)
+	{
+		printf("Domain: %s\n", virDomainGetName(domainWindows[i].domain));
+		maxNPcpus = domainWindows[i].nPcpus > maxNPcpus ? domainWindows[i].nPcpus : maxNPcpus;
+		for (int j = 0; j < domainWindows[i].nVcpus; j++)
+		{
+			printf("Vcpu%d:\n", domainWindows[i].currStats[j].number);
+			//clear mappings
+			memset(mappings, 0, VIR_CPU_MAPLEN(maxNPcpus));
+			//skip pin mapping if there is no cpu usage difference
+			unsigned long long diff = domainWindows[i].currStats[j].cpuTime - domainWindows[i].prevStats[j].cpuTime;
+			if (diff == 0)
+			{
+				continue;
+			}
+			//get next cpu pin, start at current pin setting. If it is unmapped during
+			//this round, keep it at this pin setting to avoid having to switch unneccessarily.
+			pin = getNextPcpuIndex(expectedWorkload, domainWindows[i].nPcpus, domainWindows[i].currStats[j].cpu);
+			expectedWorkload[pin] += diff;
+			cpuTimes[domainWindows[i].currStats[j].cpu] += diff;
+			//if calculated pin is the same as last pin mapped, skip the pin mapping
+			if (pin == domainWindows[i].currStats[j].cpu)
+			{
+				continue;
+			}
+			printf("pin mapping: vcpu%d -> pcpu%d\n", domainWindows[i].currStats[j].number, pin);
+			mappings[pin/8] = 1 << (pin % 8);
+			if(virDomainPinVcpu(domainWindows[i].domain, domainWindows[i].currStats[j].number, mappings, VIR_CPU_MAPLEN(domainWindows[i].nPcpus)) < 0)
+			{
+				printf("Warning! Mapping did not succeed!\n");
+			}
+			printf("maplen: %d\n", VIR_CPU_MAPLEN(domainWindows[i].nPcpus));
+		}
+		printf("\n-------------------------------------------\n");
+	}
+//	//update cpu usage table first
+//	for (int i = 0; i < nDomains; i++)
+//	{
+//		for(int j = 0; j < domainWindows[i].nVcpus; j++)
+//		{
+//			unsigned long long diff = domainWindows[i].currStats[j].cpuTime - domainWindows[i].prevStats[j].cpuTime;
+//		}
+//	}
+}
+
+int main(int argc, char **argv)
+{
+	printf("max ull: %zu\n", sizeof(unsigned long long));
+	if (argc != 2)
+	{
+		printf("wrong number of arguments...aborting.\n");
+		return -1;
+	}
+
+	int sleepTime = atoi(argv[1]);
+
 	unsigned int flags = VIR_CONNECT_LIST_DOMAINS_RUNNING |
 				VIR_CONNECT_LIST_DOMAINS_PERSISTENT;
+	//host specific variables
 	virConnectPtr conn;
-	virDomainPtr *domains;
-	virVcpuInfoPtr vcpuInfo;
-	//virNodeCPUStatsPtr *cpuStatsTable = NULL;
-	CPU_STATS cpuStats[MAX_CPUS] = {0};
-	virNodeCPUStatsPtr cpuStatsParams = NULL;
+
+	//domain specific variables
+	int nDomains = 0;
+	virDomainWindow *domainWindows = NULL;
+
+	//open connection to the hypervisor
 	conn = virConnectOpen(HYPERV_URI);
 	if (conn == NULL)
 	{
 		printf("Error connecting to Hypervisor!\n");
 	}
 	
-	nDomains = virConnectListAllDomains(conn, &domains, flags);
-	if (nDomains < 0)
+	if((nDomains = initDomainWindows(conn, domainWindows)) < 0)
 	{
-		printf("Error getting domains list.\n");
+		printf("Error initializing domain windows. Aborting.\n");
 		virConnectClose(conn);
-		//error();
+		return -1;
 	}
-	
-	printf("Number of domains: %d\n", nDomains);
-	int numCpus = virDomainGetCPUStats(domains[0], NULL, 0, 0, 0, 0);
-	printf("num cpus: %d\n", numCpus);
-	printf("vir cpu maplen: %d\n", VIR_CPU_MAPLEN(numCpus));
 
-
-	//allocate cpu stats table
-	//cpuStatsTable = (virNodeCPUStatsPtr*)calloc(numCpus, sizeof(*cpuStatsTable));
-
-	//get stats of each CPU
-	memset(cpuStats, 0, numCpus * sizeof(CPU_STATS));
-	for (int i = 0; i < numCpus; i++)
+	//initializes the beginning stats
+	if ((fetchStats(nDomains, domainWindows)) < 0)
 	{
-		//get the number of params available for the cpu stat
-		int npcpuStatsParams = 0;
-		if (virNodeGetCPUStats(conn, i, NULL, &npcpuStatsParams, 0) == 0 && npcpuStatsParams != 0)
+		printf("Error fetching VCPU stats. Aborting.\n");
+		destroyDomainWindows(nDomains, domainWindows);
+		virConnectClose(conn);
+		return -1;
+	}
+
+	while(1)
+	{
+		sleep(sleepTime);
+		printCpuMapping(nDomains, domainWindows);
+
+		if ((fetchStats(nDomains, domainWindows)) < 0)
 		{
-			printf("npcpuStatsParams%d: %d\n", i, npcpuStatsParams);
-			cpuStatsParams = (virNodeCPUStatsPtr)calloc(npcpuStatsParams, sizeof(*cpuStatsParams));
+			printf("Error fetching VCPU stats. Aborting.\n");
+			destroyDomainWindows(nDomains, domainWindows);
+			virConnectClose(conn);
+			return -1;
 		}
-
-		//parse each param
-		virNodeGetCPUStats(conn, i, cpuStatsParams, &npcpuStatsParams, 0);
-		for (int j = 0; j < npcpuStatsParams; j++)
-		{
-			printf("Field%d: %s\n", j, cpuStatsParams[j].field);
-			printf("Value: %llu\n", cpuStatsParams[j].value);
-			if (strcmp(VIR_NODE_CPU_STATS_IDLE, cpuStatsParams[j].field) == 0 ||
-				strcmp(VIR_NODE_CPU_STATS_IOWAIT, cpuStatsParams[j].field) == 0)
-			{
-				cpuStats[i].idleStats += cpuStatsParams[j].value;
-			}
-			else if (strcmp(VIR_NODE_CPU_STATS_KERNEL, cpuStatsParams[j].field) == 0 ||
-				strcmp(VIR_NODE_CPU_STATS_USER, cpuStatsParams[j].field) == 0)
-			{
-				cpuStats[i].utilizationStats += cpuStatsParams[j].value;
-			}
-				 
-		}
-		free(cpuStatsParams);
-		cpuStatsParams = NULL;
-	}
-
-	for (int i = 0; i < numCpus; i++)
-	{
-		printf("pcpu%d stats:\n", i);
-		printf("utilization: %llu\nidle: %llu\n=============\n\n", cpuStats[i].utilizationStats, cpuStats[i].idleStats);
-		printf("utilization percentage: %.2f\n-------------\n\n", (cpuStats[i].utilizationStats * 100.0)/(cpuStats[i].utilizationStats + cpuStats[i].idleStats));
-	}
-
-	for (int i = 0; i < nDomains; i++)
-	{
-		int vcpus = virDomainGetVcpusFlags(domains[i], VIR_DOMAIN_VCPU_CURRENT);
-		vcpuInfo = (virVcpuInfoPtr)calloc(vcpus, sizeof(virVcpuInfo));
-		printf("Number of vcpus: %d\n", vcpus);
-		//if (vcpus < 0)
 		
-		virDomainGetVcpus(domains[i], vcpuInfo, vcpus, NULL, 0);
-		for (int i = 0; i < vcpus; i++)
+		setNewPinMappings(nDomains, domainWindows);
+		//print cpu usage table
+		printf("CPU Usage:\n");
+		for (int i = 0; i < domainWindows[0].nPcpus; i++)
 		{
-			printf("vcpu %u:\nstate:%d\ncpuTime:%llu\ncpu:%d\n=========\n",
-				vcpuInfo[i].number, vcpuInfo[i].state, vcpuInfo[i].cpuTime, vcpuInfo[i].cpu);
+			printf("pcpu%d: %llu\n", i, cpuTimes[i]);
 		}
-		//virDomainGetVcpus(
-		free(vcpuInfo);
-		vcpuInfo = NULL;
-
-	} 
-
-	if (nDomains > 0)
-	{
-		printf("printing cpu stats\n");
-		int num_cpus = virDomainGetCPUStats(domains[0], NULL, 0, 0, 0, 0);
-		if (num_cpus < 0)
-		{
-			printf("Error: num_cpus errored out\n");
-			return 1;
-		}
-		int nparams = virDomainGetCPUStats(domains[0], NULL, 0, 0, 1, 0);
-		if (nparams < 0)
-		{
-			printf("Error, nparams errored out\n");
-			return 1;
-		}
-
-		printf("num_cpus: %d\nnparams: %d\n", num_cpus, nparams);
-		
-		virTypedParameterPtr params = NULL; 
-		params = (virTypedParameterPtr)calloc(nparams * num_cpus, sizeof(*params));
-		if (params == NULL)
-		{
-			return 1;
-		} 
-		if(virDomainGetCPUStats(domains[0], params, nparams, 0, num_cpus, 0)<0)
-		{
-			printf("Error getting CPU stats.\n");
-			return 1;
-		}
-
-		for (int i = 0; i < num_cpus; i++)
-		{
-			printf("pcpu%d stats: \n", i);
-			for (int j = 0; j < nparams; j++)
-			{
-				if (strcmp(params[i*nparams+j].field, VIR_DOMAIN_CPU_STATS_CPUTIME) == 0)
-				{
-					printf ("cpu_time: %llu\n", params[i].value.ul);
-				}
-				else if (strcmp(params[i*nparams+j].field, VIR_DOMAIN_CPU_STATS_VCPUTIME) == 0)
-				{
-					printf("vcpu_time: %llu\n", params[i].value.ul);
-				}
-			}
-		}
-
-		free(params);
 	}
+
+	destroyDomainWindows(nDomains, domainWindows);
 	virConnectClose(conn);
-	
-	for (int i = 0; i < nDomains; i++)
-	{
-		virDomainFree(domains[i]);
-	}
+	return 0;
 }
